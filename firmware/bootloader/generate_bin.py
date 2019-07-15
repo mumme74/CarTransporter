8c7c1f48e6cb0d264dee2e45ffd8d2e30c5473dd
Fredrik Johansson <fredrik@mumme.se>
2019-07-15 04:46:20 +0200

    Add a script that creates a bin with nice numbers to view. So we now were it went wrong during flashing

---
+++ firmware/bootloader/generate_bin.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python
+import sys
+
+def create(sz = 51200):
+    i =0
+    with open("MOCKUP.bin", "wb") as f:
+        f.write(i.to_bytes(4, "little")) # first 4 bytes is startup code, set to zero
+        while sz > 0:
+            sz -= 1
+            f.write(i.to_bytes(1, "little"))
+            i += 1
+            if (i > 0xf):
+                i = 0
+        print("generated MOCKUP.bin")
+                
+                
+                
+if (__name__ == "__main__"):
+    if (len(sys.argv) == 2):
+       try:
+            sz = int(sys.argv[1])
+            create(sz)
+       except:
+            create()
+
